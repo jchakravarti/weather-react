ed85bda2ab406312787692488a650940d4709a6e
jchakravarti <79936713+jchakravarti@users.noreply.github.com>
2021-04-25 23:25:57 +0100

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export default function Weather(props) {
+
+    function handleResponse(response) {
+        alert(`The weather in ${props.city} is ${Math.round(response.data.main.temp)}`)
+    }
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=98ba3df7ef91e3454412bee7a0392f55&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (
+        <div className="container">
+            <form>
+                <input type="search" />
+                <input type="submit" className="btn btn-secondary"/>
+            </form>
+            <h1>
+                London
+            </h1>
+            <h2>
+                Sunday 14:00
+            </h2>
+            <h3>
+                Sunny
+            </h3>
+            <div className="row">
+                <div className="col-sm-1">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+                </div>
+                <div className="col-sm-1">
+                    13°C
+                </div>
+            </div>
+        </div>
+    )
+}
